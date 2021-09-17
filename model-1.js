62bdb6a2db4e11ced07b6a1c6a1e8b2bad4bbd8c
Dillon Parker <prkr93@gmail.com>
2021-09-16 20:17:19 -0600

    Add objects to model-1.js

--- model-1.js
+++ model-1.js
@@ -2,6 +2,7 @@ var schoolName = "Turing";
 var schoolProgram = "Front End";
 var startMonth = "November";
 
+
 var monthsLong = 7;
 var tuition = 20000;
 
@@ -9,5 +10,9 @@ var tuition = 20000;
 var enrolled = true;
 var feelingConfident = true;
 
+
 var strategies = ["Calendaring", "Review"];
 var toDo = ["Technical Assesment", "Homework", "Mod 1 Prework"];
+
+
+var exampleChair = ["Brown", 3, true];
